d8a214a49f2a5698e584bfe8c698c961f78e9215
sdroyds <mikew8978@gmail.com>
2016-03-26 04:57:29 -0400

    Lockscreen weather options

---
+++ src/com/android/settings/solo/WeatherSettings.java
@@ -0,0 +1,155 @@
+/* 
+ * Copyright (C) 2016 Tuesday-Rom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.solo;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.os.Bundle;
+import android.preference.SwitchPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+public class WeatherSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String PREF_SHOW_WEATHER =
+            "weather_show_weather";
+    private static final String PREF_SHOW_LOCATION =
+            "weather_show_location";
+    private static final String PREF_SHOW_TIMESTAMP =
+            "weather_show_timestamp";
+    private static final String PREF_CONDITION_ICON =
+            "weather_condition_icon";
+
+    private static final int MONOCHROME_ICON = 0;
+
+    private SwitchPreference mShowWeather;
+    private SwitchPreference mShowLocation;
+    private SwitchPreference mShowTimestamp;
+    private ListPreference mConditionIcon;
+
+    private ContentResolver mResolver;
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.TUNER;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        refreshSettings();
+    }
+
+    public void refreshSettings() {
+        PreferenceScreen prefs = getPreferenceScreen();
+        if (prefs != null) {
+            prefs.removeAll();
+        }
+
+        addPreferencesFromResource(R.xml.weather);
+        mResolver = getActivity().getContentResolver();
+
+        boolean showWeather = Settings.System.getInt(mResolver,
+                Settings.System.LOCK_SCREEN_SHOW_WEATHER, 0) == 1;
+        int conditionIcon = Settings.System.getInt(mResolver,
+               Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, MONOCHROME_ICON);
+
+        mShowWeather =
+                (SwitchPreference) findPreference(PREF_SHOW_WEATHER);
+        mShowWeather.setChecked(showWeather);
+        mShowWeather.setOnPreferenceChangeListener(this);
+
+        if (showWeather) {
+            mShowLocation =
+                    (SwitchPreference) findPreference(PREF_SHOW_LOCATION);
+            mShowLocation.setChecked(Settings.System.getInt(mResolver,
+                    Settings.System.LOCK_SCREEN_SHOW_WEATHER_LOCATION, 1) == 1);
+            mShowLocation.setOnPreferenceChangeListener(this);
+
+            mShowTimestamp =
+                    (SwitchPreference) findPreference(PREF_SHOW_TIMESTAMP);
+            mShowTimestamp.setChecked(Settings.System.getInt(mResolver,
+                    Settings.System.LOCK_SCREEN_SHOW_WEATHER_TIMESTAMP, 1) == 1);
+            mShowTimestamp.setOnPreferenceChangeListener(this);
+
+            mConditionIcon =
+                    (ListPreference) findPreference(PREF_CONDITION_ICON);
+            mConditionIcon.setValue(String.valueOf(conditionIcon));
+            mConditionIcon.setSummary(mConditionIcon.getEntry());
+            mConditionIcon.setOnPreferenceChangeListener(this);
+
+        } else {
+            removePreference(PREF_SHOW_LOCATION);
+            removePreference(PREF_SHOW_TIMESTAMP);
+            removePreference(PREF_CONDITION_ICON);
+        }
+        if (showWeather && ((conditionIcon == MONOCHROME_ICON)
+                || (conditionIcon != MONOCHROME_ICON ))) {
+        }
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean value;
+        if (preference == mShowWeather) {
+            value = (Boolean) newValue;
+            Settings.System.putInt(mResolver,
+                    Settings.System.LOCK_SCREEN_SHOW_WEATHER,
+                    value ? 1 : 0);
+            refreshSettings();
+            return true;
+        } else if (preference == mShowLocation) {
+            value = (Boolean) newValue;
+            Settings.System.putInt(mResolver,
+                    Settings.System.LOCK_SCREEN_SHOW_WEATHER_LOCATION,
+                    value ? 1 : 0);
+            return true;
+        } else if (preference == mShowTimestamp) {
+            value = (Boolean) newValue;
+            Settings.System.putInt(mResolver,
+                    Settings.System.LOCK_SCREEN_SHOW_WEATHER_TIMESTAMP,
+                    value ? 1 : 0);
+            return true;
+        } else if (preference == mConditionIcon) {
+            int intValue = Integer.valueOf((String) newValue);
+            int index = mConditionIcon.findIndexOfValue((String) newValue);
+            Settings.System.putInt(mResolver,
+                    Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, intValue);
+            mConditionIcon.setSummary(mConditionIcon.getEntries()[index]);
+            refreshSettings();
+            return true;
+        }
+        return false;
+    }
+}
+
