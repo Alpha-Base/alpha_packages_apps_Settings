tornado76/android_packages_apps_Settings

Settings: forward port HeadsUp options (3/4) * SwitchBar Global on/off * Blacklist, Whitelist and Do not disturb
PureNexus Edits:
*added back SwitchBar
*added BaseGlobalSwitchBar
*made launchable externally
*cleanup/polish

src/com/android/settings/SettingsActivity.java
@@ -82,6 +82,7 @@ import com.android.settings.applications.WriteSettingsDetails;
 import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.nexus.DisplayRotation;
+import com.android.settings.nexus.HeadsUpSettings;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
 import com.android.settings.dashboard.DashboardTile;
@@ -365,7 +366,8 @@ public class SettingsActivity extends Activity
             NotificationLightSettings.class.getName(),
             ExpandedDesktopPreferenceFragment.class.getName(),
             DisplayRotation.class.getName(),
-            BlacklistSettings.class.getName()
+            BlacklistSettings.class.getName(),
+            HeadsUpSettings.class.getName()
     };
 
 

Alpha-Base/alpha_packages_apps_Settings

src/com/android/settings/SettingsActivity.java
@@ -79,6 +79,7 @@ import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.WriteSettingsDetails;
 import com.android.settings.bluetooth.BluetoothSettings;
+import com.android.settings.nexus.HeadsUpSettings;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
 import com.android.settings.dashboard.DashboardTile;
@@ -365,7 +366,8 @@ public class SettingsActivity extends Activity
             Statusbar.class.getName(),
             NavbarSettings.class.getName(),
             FlingSettings.class.getName(),
-            SmartbarSettings.class.getName()
+            SmartbarSettings.class.getName(),
+            HeadsUpSettings.class.getName()
     };
 
 